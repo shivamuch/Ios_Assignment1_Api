ebfdb0324728e2eb456b20dd74ba722eb39382a9
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-02-08 18:21:52 -0500

    gave support to functions of IBAction function for the API

--- Ios_Assignment1_API/ViewController.swift
+++ Ios_Assignment1_API/ViewController.swift
@@ -17,7 +17,25 @@ class ViewController: UIViewController {
 
 
     
-   
+    @IBAction func kitsu(_ sender: UIButton) {
+        let url = "https://kitsu.io/api/edge/anime";
+        
+        APIDataFetch().fetchData(url: url);
+        
+    }
+    
+    @IBAction func dailymotion(_ sender: UIButton) {
+        let url = "https://api.dailymotion.com/channels?fields=created_time%2Cdescription%2Cname%2Cupdated_time";
+        
+        APIDataFetch().fetchData(url: url);
+        
+    }
+    
+    @IBAction func fishWatch(_ sender: UIButton) {
+        let url = "https://www.fishwatch.gov/api/species";
+
+        APIDataFetch().fetchData(url: url);
+    }
 }
 
 
