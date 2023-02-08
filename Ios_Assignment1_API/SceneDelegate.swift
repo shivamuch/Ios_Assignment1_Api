9660e9a60a686085fdc20bdf1862faaddfcaf42a
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-02-08 18:12:54 -0500

    Update SceneDelegate.swift
    functions for disconnect and active state

--- Ios_Assignment1_API/SceneDelegate.swift
+++ Ios_Assignment1_API/SceneDelegate.swift
@@ -19,7 +19,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
-   
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    
+
+
 }
 
 
