14fb5292c87332b9cddd33fff8bc5aa49260365c
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-02-08 17:49:47 -0500

    Scene delegate
    Added new class for scenes

---
+++ Ios_Assignment1_API/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  Ios_Assignment1_API
+//
+//  Created by Shivam Bajaj on 2023-02-08.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+}
