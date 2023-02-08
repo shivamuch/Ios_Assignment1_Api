752eef96464edb8f02dcd24d110de8772b38a8e4
Shivam Bajaj <shivam.bajaj1609@gmail.com>
2023-02-08 17:07:30 -0500

    Initial Commit
    Added project files to the repository.

---
+++ Ios_Assignment1_API/Ios_Assignment1_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ios_Assignment1_APIApp.swift
+//  Ios_Assignment1_API
+//
+//  Created by Shivam Bajaj on 2023-02-08.
+//
+
+import SwiftUI
+
+@main
+struct Ios_Assignment1_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
